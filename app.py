b1624dd77d4474c71b864b3f88d6762558468c6c
Kanti Sonnathi <sonnathikanti@gmail.com>
2021-09-20 16:51:59 +0530

    meet 1

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
+from flask import request
 from flask import Flask
+from models import Blockchain, Block
 
 app = Flask(__name__)
+blockchain = Blockchain()
 
 
 @app.route('/transactions/new', methods=['POST'])
 def create_new_transaction():
-    return 'creating new transaction'
+    new_transaction = request.get_json()
+    blockchain.unconfirmed_transactions.append(new_transaction)
+    print('values are :', new_transaction)
+    return 'transaction added to unverified pool'
 
 
-@app.route('transactions/view', methods=['GET'])
+@app.route('/transactions/view', methods=['GET'])
 def view_all_transactions():
-    return 'this method is supposed to return a list of all the transactions'
+    chain = blockchain.chain
+    ret = ''
+    for current in chain:
+        ret += str(current.index) + ' '
+    print('current unverified transactions are: ')
+    print(blockchain.unconfirmed_transactions)
+    return ret
+
+
+@app.route('/transactions/unverified', methods=['GET'])
+def view_unverified_transactions():
+    return str(blockchain.unconfirmed_transactions)
+
+
+@app.route('/mine')
+def mine_block():
+    return 'not mined yet. hang tight :)'
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,17 +1,38 @@
+import json
+from hashlib import sha256
+
+import time
+
+
 class Blockchain:
-    # list of blocks (let each block have more than one transaction to show ma'am flexibility?)
-    def __init__(self, genesis_block):
-        self.genesis_block = genesis_block
-        self.height = 0
-        # figure out the structure or whatever
+    def __init__(self):
+        self.unconfirmed_transactions = []
+        self.chain = []
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        genesis_block = Block(0, [], time.time(), "0")
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
 
 
 class Block:
-    def __init__(self, prev_hash, transactions, index, ):
-        # needs to have a list of transactions
-        # needs to have index, date, time and (proof?)
-        # also needs to have previous hash.
-        # timestamp
+    def __init__(self, index, transactions, timestamp, previous_hash, nonce=0):
+        self.index = index
         self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
+        self.nonce = nonce
+
+    def __repr__(self):
+        return 'index: ' + self.index + ', transactions: ' + self.transactions + ', timestamp: ' + self.timestamp
+
+    def compute_hash(self):
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        return sha256(block_string.encode()).hexdigest()
 
 
